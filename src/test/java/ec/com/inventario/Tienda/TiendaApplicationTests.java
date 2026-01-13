6d36101654ff59e9e8ce3f09df34620ab74b2841
Joel2775 <joel.acosta@epn.edu.ec>
2026-01-12 19:47:55 -0500

    Inicializacion del proyecto con SpringBoot

---
+++ src/test/java/ec/com/inventario/Tienda/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.inventario.Tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

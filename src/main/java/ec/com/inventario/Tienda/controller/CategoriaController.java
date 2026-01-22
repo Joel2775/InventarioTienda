c8cf25ae8fbb1590b741b5b8659f9b3e525ead58
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-01-22 00:20:12 -0500

    CategoriaController

---
+++ src/main/java/ec/com/inventario/Tienda/controller/CategoriaController.java
@@ -0,0 +1,20 @@
+package ec.com.inventario.Tienda.controller;
+
+import ec.com.inventario.Tienda.model.dto.ProductoDTO;
+import ec.com.inventario.Tienda.service.ICategoriaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/inventario")
+public class CategoriaController {
+    private final ICategoriaService categoriaService;
+
+    public CategoriaController(ICategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+    
+}

52857d9ed6ed2c3417701bfcea5f0409b2fd9fb1
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-01-22 23:39:31 -0500

    Modificando el CategoriaController

--- src/main/java/ec/com/inventario/Tienda/controller/CategoriaController.java
+++ src/main/java/ec/com/inventario/Tienda/controller/CategoriaController.java
@@ -2,8 +2,6 @@ package ec.com.inventario.Tienda.controller;
 
 import ec.com.inventario.Tienda.model.dto.ProductoDTO;
 import ec.com.inventario.Tienda.service.ICategoriaService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,29 +16,30 @@ public class CategoriaController {
     }
 
     @GetMapping("/categorias")
-    public ResponseEntity<List<ProductoDTO>> getAll(){
-        return ResponseEntity.ok(categoriaService.listarTodas());
+    public List<ProductoDTO> getAll(){
+        return categoriaService.getCategoria();
     }
 
     @GetMapping("/categorias/{id}")
-    public ResponseEntity<ProductoDTO> getById(@PathVariable Long id){
-        return ResponseEntity.ok(categoriaService.obtenerPorId(id));
+    public ProductoDTO getById(@PathVariable Long id){
+        return categoriaService.findCategoria(id);
     }
 
     @PostMapping("/categorias")
-    public ResponseEntity<Void> save(@RequestBody ProductoDTO categoria){
-        categoriaService.crear(categoria);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public String save(@RequestBody ProductoDTO categoriasDTO){
+        categoriaService.crearCategoria(categoriasDTO);
+        return "La categoria fue creada correctamente";
     }
+
     // Actualizar solo un campo
-    @PatchMapping("/categorias/{id}")
-    public ResponseEntity<ProductoDTO> update(@PathVariable Long id, @RequestBody ProductoDTO categoria){
-        return ResponseEntity.ok(categoriaService.actualizar(id, categoria));
+    @PatchMapping("/{id}")
+    public ProductoDTO update(@PathVariable Long id, @RequestBody ProductoDTO categorias){
+        return categoriaService.actualizarCategoria(id, categorias);
     }
 
     @DeleteMapping("/categorias/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        categoriaService.eliminar(id);
-        return ResponseEntity.noContent().build();
+    public String delete(@PathVariable Long id){
+        categoriaService.eliminarCategoria(id);
+        return "La categoria fue eliminada correctamente";
     }
 }

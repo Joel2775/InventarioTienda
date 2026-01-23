9dd508f250454f0066c857b78e0768ca74c8de47
Joel2775 <joel.acosta@epn.edu.ec>
2026-01-22 22:21:42 -0500

    implementacion prueba de excepciones

---
+++ src/main/java/ec/com/inventario/Tienda/exception/ListaVaciaException.java
@@ -0,0 +1,7 @@
+package ec.com.inventario.Tienda.exception;
+
+public class ListaVaciaException extends RuntimeException {
+    public ListaVaciaException(String message) {
+        super(message);
+    }
+}

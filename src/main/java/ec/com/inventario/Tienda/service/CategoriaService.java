f6f8f11be68279b3b9e8471ccb295d596a493936
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-01-23 21:26:30 -0500

    CategoriaService.java

--- src/main/java/ec/com/inventario/Tienda/service/CategoriaService.java
+++ src/main/java/ec/com/inventario/Tienda/service/CategoriaService.java
@@ -1,14 +1,10 @@
 package ec.com.inventario.Tienda.service;
 
-import ec.com.inventario.Tienda.model.dto.ProductoDTO;
 import ec.com.inventario.Tienda.model.entity.Categorias;
-import ec.com.inventario.Tienda.model.entity.Producto;
 import ec.com.inventario.Tienda.repository.ICategoriasRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
 
 @Service
 public class CategoriaService implements ICategoriaService {
@@ -20,71 +16,45 @@ public class CategoriaService implements ICategoriaService {
     }
 
     @Override
-    public List<ProductoDTO> getCategoria() {
-        List<Categorias>listaCategorias = categoriasRepository.findAll();
-        return listaCategorias.stream()
-                .map(categorias -> pasarACategoriaDTO(categorias))
-                .collect(Collectors.toList());
+    public List<Categorias> getCategoria(){
+        List<Categorias>listarCategorias =categoriasRepository.findAll();
+        return listarCategorias;
     }
 
     @Override
-    public ProductoDTO findCategoria(Long id) {
-        Categorias categorias = categoriasRepository.findById(id).orElse(null);
-        return pasarACategoriaDTO(categorias);
+    public Categorias findCategoria(Long id){
+        return categoriasRepository.findById(id).orElse(null);
     }
 
-
     @Override
-    public void crearCategoria(ProductoDTO categoriaDTO) {
-        Categorias categorias = categoriasRepository.findById(categoriaDTO.getProductoId()).orElse((null));
+    public void crearCategoria(Categorias categorias){
         categoriasRepository.save(categorias);
     }
 
 
     @Override
-    public ProductoDTO actualizarCategoria(Long id, ProductoDTO datosNuevos) {
+    public Categorias actualizarCategoria(Long id, Categorias datosNuevos){
         Categorias categoriaExistente = categoriasRepository.findById(id).orElse(null);
 
         if(categoriaExistente == null){
             return null;
         }
 
-        // 2. Actualizar los campos que vienen con valor
+        if(datosNuevos.getCategoriaId() != null){
+            categoriaExistente.setCategoriaId((datosNuevos.getCategoriaId()));
+        }
+
         if(datosNuevos.getCategoriaNombre() != null){
             categoriaExistente.setCategoriaNombre(datosNuevos.getCategoriaNombre());
         }
 
-        // 3. Guardar y retornar
-        Categorias actualizado = categoriasRepository.save(categoriaExistente);
-        return  pasarACategoriaDTO(actualizado);
+        return categoriasRepository.save(categoriaExistente);
     }
 
     @Override
-    public void eliminarCategoria(Long id) {
+    public void eliminarCategoria(Long id){
         categoriasRepository.deleteById(id);
     }
-
-
-    public ProductoDTO pasarACategoriaDTO(Categorias categoria) {
-        if (categoria == null) {
-            return null;
-        }
-
-        ProductoDTO dto = new ProductoDTO();
-        dto.setCategoriaId(categoria.getCategoriaId());
-        dto.setCategoriaNombre(categoria.getCategoriaNombre());
-
-        return dto;
-    }
-
-    public Categorias pasarACategoriaEntidad(ProductoDTO dto) {
-        if (dto == null) {
-            return null;
-        }
-
-        Categorias categoria = new Categorias();
-        categoria.setCategoriaNombre(dto.getCategoriaNombre());
-
-        return categoria;
-    }
 }
+
+

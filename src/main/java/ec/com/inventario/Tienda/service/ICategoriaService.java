dbc0bd112a25d7e0466079a039392c12a9eb4340
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-01-23 21:26:00 -0500

    ICategoriaService

--- src/main/java/ec/com/inventario/Tienda/service/ICategoriaService.java
+++ src/main/java/ec/com/inventario/Tienda/service/ICategoriaService.java
@@ -1,21 +1,22 @@
 package ec.com.inventario.Tienda.service;
 
-import ec.com.inventario.Tienda.model.dto.ProductoDTO;
+import ec.com.inventario.Tienda.model.entity.Categorias;
+
 import java.util.List;
 
 public interface ICategoriaService {
 
     // OBTENER TODAS LAS CATEGORÍAS
-    public List<ProductoDTO> getCategoria();
+    public List<Categorias> getCategoria();
 
     // OBTENER UNA CATEGORÍA POR ID
-    public ProductoDTO findCategoria(Long id);
+    public Categorias findCategoria(Long id);
 
     // CREAR UNA NUEVA CATEGORÍA
-    public void crearCategoria(ProductoDTO categoriaDTO);
+    public void crearCategoria(Categorias categorias);
 
     // ACTUALIZAR UNA CATEGORÍA EXISTENTE
-    public ProductoDTO actualizarCategoria(Long id, ProductoDTO datosNuevos);
+    public Categorias actualizarCategoria(Long id, Categorias datosNuevos);
 
     // ELIMINAR UNA CATEGORÍA POR ID
     public void eliminarCategoria(Long id);

5292888f8c53cb75dee0dd65edbd627d0915e40a
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2026-01-25 18:23:23 -0500

    ICategoriaService.java

--- src/main/java/ec/com/inventario/Tienda/service/ICategoriaService.java
+++ src/main/java/ec/com/inventario/Tienda/service/ICategoriaService.java
@@ -3,7 +3,7 @@ package ec.com.inventario.Tienda.service;
 import ec.com.inventario.Tienda.model.dto.CategoriaCreateDTO;
 import ec.com.inventario.Tienda.model.dto.CategoriaResponseDTO;
 import ec.com.inventario.Tienda.model.dto.CategoriaUpdateDTO;
-import ec.com.inventario.Tienda.model.entity.Categorias;
+
 
 import java.util.List;
 
@@ -21,6 +21,4 @@ public interface ICategoriaService {
     // ACTUALIZAR UNA CATEGORÍA EXISTENTE
     public CategoriaResponseDTO actualizarCategoria(Long id, CategoriaUpdateDTO categoriaUpdateDTO);
 
-    // ELIMINAR UNA CATEGORÍA POR IDY
-    // public void eliminarCategoria(Long id);
 }
